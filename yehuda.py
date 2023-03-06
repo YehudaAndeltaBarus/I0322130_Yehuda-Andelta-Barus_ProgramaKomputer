302f0cc9a5ee59d8f2af9b7a40dadf121e5cd497
Yehuda <yehudaandeltab@student.uns.ac.id>
2023-03-06 10:03:41 +0700

    commit pertama

---
+++ yehuda.py
@@ -0,0 +1,14 @@
+nama__depan =  "yehuda"
+nama__blk = "barus"
+
+angka = 10
+desimal = 0,5
+boolean = True
+kelas_D = ["yehuda","kelas  D","dll"]
+dictionary = {"nama": "yehuda", "kelas":"KLS D",}
+
+print(nama__depan)
+print(nama__blk)
+print("nama saya adalah yehuda")
+
+print()
