c5b362eda448da2c3980fe6b53d490b5b36aef53
Yehuda <yehudaandeltab@student.uns.ac.id>
2023-03-12 21:53:36 +0700

    Exersice-1

---
+++ Exersice-1.py
@@ -0,0 +1,13 @@
+# Tugas 1 Praktikum Prokom
+
+nama = "Yehuda Andelta Barus"
+nim = "I0322130"
+tempat_tinggal = "Kabanjahe"
+visualisasi = "Rumah saya berada di Kabanjahe, jika divisualisasikan Kabanjahe berada di dataran tinggi jadi cuaca dingin, sehingga cocok menjadi tempat pertanian. Kabanjahe juga dikenal sebagai daerah pertanian sehingga banyak sekali lahan pertanian di Kabanjahe. Salah satu tumbuhan yang terkenal yaitu buah jeruk Kabanjahe. Persekitaran rumah saya juga masih banyak lahan pertanian sehingga tampak hijau dan cuacanya dingin."
+harapan = "Harapan saya sebagai lulusan Teknik Industri adalah dapat menjadi seorang sistem analis nantinya. Karena saya melihat bahwa seorang data analis sangat keren dan juga memilki gaji yang sangat baik."
+
+print("Nama saya adalah", nama)
+print("Nim saya", nim)
+print("Saya berasal dari", tempat_tinggal)
+print(visualisasi)
+print(harapan)
